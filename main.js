4a9ed1c169c70032865b546fbe0faf8d3cac25c0
Baxromov Islombek <islom@mail.ru>
2023-04-27 19:21:53 +0500

    add audio

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 function onScanSuccess(qrCodeMessage) {
-    alert(qrCodeMessage);
-    document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
+    if (qrCodeMessage == 'Shoxa') {
+        playOn();
+        document.getElementById('result').innerHTML = '<span class="result">' + qrCodeMessage + '</span>';
+    } else {
+        playOFF();
+        document.getElementById('result').innerHTML = '<span class="result">' + qrCodeMessage + '</span>';
+    }
 }
 
 function onScanError(errorMessage) {
-  //handle scan error
+    //handle scan error
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+
+let playOn = () => new Audio("audio/on.mp3").play();
+let playOFF = () => new Audio("audio/off.mp3").play();
